1407c695269530626e53729aa769e7c97304a292
Gowri M S <msgowri2006@gmail.com>
2026-06-15 14:22:21 +0530

    fix: dynamic base for vercel and github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-task-tracker-pro/', // ← ITHU ADD CHEYYANAM
+  base: process.env.NODE_ENV === 'production' ? '/react-task-tracker-pro/' : '/',
   plugins: [react()],
 })
