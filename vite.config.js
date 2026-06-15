e0f28967b38a380c6669a6af9eafd2983059a428
gowrims2006 <msgowri69@gmail.com>
2026-06-15 13:59:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/react-task-tracker-pro/', // ← ITHU ADD CHEYYANAM
   plugins: [react()],
-  base: '/',
 })
